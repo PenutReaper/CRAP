cda7135d225f602c25dede28ea2a7da7bedd6d9f
James Hill <James-Hill2016@outlook.com>
2017-04-02 03:03:15 +0100

    Add basic content loading

--- CRAP/Program.cs
+++ CRAP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,14 +16,54 @@ namespace CRAP
             while (true)
             {
                 // @Temp Need a better way of limiting fps???
-                if(System.DateTime.Now.Millisecond % 16 == 0)
+                if(System.DateTime.Now.Millisecond % 100 == 0)
                 Primary_Renderer.Render();
+
+                if (Primary_Renderer.Ticks() >= 30)
+                {
+                    Primary_Renderer.Load_Content("TEST");
+                }
             }
         }
 
         static void Init_Console_Renderer(int Width, int Height)
         {
             Primary_Renderer.Set_Properties(Width, Height);
+        }
+    }
+
+    class Content_Loader
+    {
+        enum Load_Modes
+        {
+            Debug,
+            Static,
+            Dynamic
+        };
+
+        Load_Modes Load_Mode = Load_Modes.Debug;
+
+        public void Load()
+        {
+            if (Load_Mode.Equals(Load_Modes.Debug)) Load_Debug();
+            if (Load_Mode.Equals(Load_Modes.Static)) Load_Static();
+            if (Load_Mode.Equals(Load_Modes.Dynamic)) Load_Dynamic();
+            return;
+        }
+
+        private void Load_Dynamic()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Load_Static()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Load_Debug()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- CRAP/Render_View.cs
+++ CRAP/Render_View.cs
@@ -7,6 +7,9 @@ namespace CRAP
     {
         Render_View_Propeties Properties = new Render_View_Propeties();
         Render_Content Content = new Render_Content();
+
+        public long Ticks() => Properties.Ticks;
+        public void Load_Content(string New_Content) => Content.Load(New_Content); 
         
         public void Set_Properties(int Width, int Height)
         {
@@ -18,19 +21,12 @@ namespace CRAP
         public String Render_Setup()
         {
             Content.Update(Properties.Width, Properties.Height);
-            string Render_String = "";
-
-            foreach (var item in Content.Data)
-            {
-                Render_String += item.ToString();
-            };
-
-            return Render_String;
+            return Content.Get_Data();
         }
 
         public void Render()
         {
-            string To_Render = Render_Setup();
+            string To_Render = Properties.Ticks == 0 ? Render_Setup() : Content.Get_Data();
             Properties.Ticks += 1;
 
             Console.Clear();
