ffa348c3ad6fbb8beb29243e891f98e02925c715
James Hill <James-Hill2016@outlook.com>
2017-05-02 17:36:13 +0100

    Started complete rewrite. I was going too OOP. Screw OOP. It sucks.

--- CRAP/Program.cs
+++ CRAP/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,63 +6,24 @@ namespace CRAP
 {
     class Program
     {
-        static Render_View Primary_Renderer = new Render_View();
-                
+        static bool _exit_flag = false;
+        static int _cur_tick = 1;
+
         static void Main(string[] args)
-        {
-            Init_Console_Renderer(80, 45);
-
-            while (true)
+        {            
+            while (!_exit_flag)
             {
-                // @Temp Need a better way of limiting fps???
-                if(System.DateTime.Now.Millisecond % 100 == 0)
-                Primary_Renderer.Render();
+                if (_cur_tick >= 10) { _exit_flag = true; }
 
-                if (Primary_Renderer.Ticks() >= 30)
-                {
-                    Primary_Renderer.Load_Content("TEST");
-                }
+                _cur_tick++;
             }
+
+            InChar();
         }
 
-        static void Init_Console_Renderer(int Width, int Height)
+        static char InChar()
         {
-            Primary_Renderer.Set_Properties(Width, Height);
-        }
-    }
-
-    class Content_Loader
-    {
-        enum Load_Modes
-        {
-            Debug,
-            Static,
-            Dynamic
-        };
-
-        Load_Modes Load_Mode = Load_Modes.Debug;
-
-        public void Load()
-        {
-            if (Load_Mode.Equals(Load_Modes.Debug)) Load_Debug();
-            if (Load_Mode.Equals(Load_Modes.Static)) Load_Static();
-            if (Load_Mode.Equals(Load_Modes.Dynamic)) Load_Dynamic();
-            return;
-        }
-
-        private void Load_Dynamic()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Load_Static()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Load_Debug()
-        {
-            throw new NotImplementedException();
+            return System.Console.ReadKey().KeyChar;
         }
     }
 }
